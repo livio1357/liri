8e69374dc884e979051729f59f22858448ed2128
livio bolzon <livio@actingstudiochicago.com>
2017-07-23 11:08:29 -0500

    push more

--- keys.js
+++ keys.js
@@ -1,12 +1,21 @@
 var Twitter = require('twitter');
-
-
+var Spotify = require('node-spotify-api');
+//twitter
 var consum = new Twitter({
-    consumer_key: 'dgyhsTiw72ccOEY9KTZqoJZML',
-    consumer_secret: '	jQBmAWhPgGgbt2BmUvCGOcXdaLvoFEcx9OogQUhtNJw2x4W05Q',
-    access_token_key: '	746411886552309760-ifTI9rEcX3x5SvxD9jNffiGI8WcKjSC',
-    access_token_secret: '	AsvbNMD1VflxDkn0Naadr5WHXhzmoTSJLcmZMzBnpMvRl'
+    consumer_key: '0cbq656xYKESPT9h7sO3CIeCf',
+    consumer_secret: 'Rmn5kzW0It4D5yPsnS9u3nB4ttdY6aDjLbqH0JSOc8mXV9UyDK',
+    access_token_key: '746411886552309760-ifTI9rEcX3x5SvxD9jNffiGI8WcKjSC',
+    access_token_secret: 'AsvbNMD1VflxDkn0Naadr5WHXhzmoTSJLcmZMzBnpMvRl'
 });
 
 
+// spotify
+
+var spotify = new Spotify({
+  id: '539f4fcc9ce24201b51c39bc8745d851',
+  secret: 'a311e7086a274595b014cda72494e32c'
+});
+
+//export everywhere
 module.exports.consum = consum;
+module.exports.spotify = spotify;
--- liri.js
+++ liri.js
@@ -1,25 +1,39 @@
 var Twitter = require('twitter');
 
+var spotify = require("spotify");
+
 var fs = require('fs');
 
 // get keys 
-var keys = require('./keys.js');
+var Keys = require('./keys.js');
 
 
-var twitterConsum = keys.consum
+var twitterconsum = Keys.consum
 
-var command = process.argv[2];
+//commands
 
-if (command === 'my-tweets') {
-	
+console.log("Please Type one of the following: my-tweets, spotify-song, movie-please")
+
+
+// search / commands
+
+var mytweets = process.argv[2];
+
+var search = process.argv[3];
+
+
+//command to type after node liri.js my-tweets
+
+if (mytweets === 'my-tweets') {
+
   var name = {
     screen_name: "codemoji123"
 };
 
 
- twitterConsum.get("statuses/user_timeline", name, function(error, tweets, response) {
+ twitterconsum.get("statuses/user_timeline", name, function(error, tweets, response) {
     if (!error) {
-      //Loop through each tweet inside response data
+      
       for (var i = 0; i < tweets.length; i++) {
         console.log(`\nTweet ${i + 1}: ${tweets[i].text}`);
         console.log(`Created On: ${tweets[i].created_at}`);
@@ -29,4 +43,37 @@ if (command === 'my-tweets') {
       console.log(error);
     }
   });
+}
+
+
+function spotifyNow(){
+  console.log("Its time to party with some music");
+
+  var searchMusic;
+  if(search === undefined){
+    console.log("Please try again with something that we really do have in our system"); 
+  }
+  else{
+    searchMusic = search; //Finds user input
+  }
+
+  spotify.search({type: "track", query: searchMusic}, function(err , data){
+    if(err){
+      console.log(err);
+      return;
+    } else if (data.error) {
+      console.log(data);
+      return;
+    }
+
+    else{
+      console.dir(data);
+      // console.log("Artist: " + data.tracks.items[0].artists[0].name);
+      // console.log("Song: " + data.tracks.items[0].name);
+      // console.log("Album: " + data.tracks.items[0].album.name);
+      // console.log("Preview here: " + data.tracks.items[0].preview_url);
+    }
+
+  })
+
 }
